2ff971172f78c808085f006319b410ccd59f7898
davidcatalan9 <dcatalan@gmail.com>
2023-08-23 21:42:24 -0400

    entrega final david catalan

--- loginSistema/views.py
+++ loginSistema/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
 from django.contrib.auth import authenticate, login
 
 def login_view(request):
@@ -22,3 +23,15 @@ def logout_view(request):
     logout(request)
     return redirect('inicio')
 
+
+def registro_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # Guarda el nuevo usuario en la base de datos
+            login(request, user)  # Inicia sesión automáticamente después del registro
+            return redirect('inicio')  # Redirige a otra vista después del registro
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'registro.html', {'form': form})
